9a7d28138e0b1d37da3f089154d7ab44f8163d7b
Furkan Bulut <furkanbtng@gmail.com>
2023-03-12 01:03:01 +0300

    File uploaded.

---
+++ main.py
@@ -0,0 +1,620 @@
+import turtle
+t = turtle.Turtle()
+t1= turtle.Turtle()
+t2 = turtle.Turtle()
+t3 = turtle.Turtle()
+t4 = turtle.Turtle()
+t5 = turtle.Turtle()
+t6 = turtle.Turtle()
+t7 = turtle.Turtle()
+t8 = turtle.Turtle()
+t9= turtle.Turtle()
+t10 = turtle.Turtle()
+t11 = turtle.Turtle()
+t12 = turtle.Turtle()
+t13 = turtle.Turtle()
+
+
+w = turtle.Screen()
+w.bgcolor("black")
+
+turtle = turtle.Turtle()
+
+def temizle():
+    t.reset()
+    t1.reset()
+    t2.reset()
+    t3.reset()
+    t4.reset()
+    t5.reset()
+    t6.reset()
+    t7.reset()
+    t1.clear()
+    t.clear()
+    t2.clear()
+    t3.clear()
+    t4.clear()
+    t5.clear()
+    t6.clear()
+    t7.clear()
+
+def d():
+    t.speed(20)
+    t.pensize(30)
+    t.setpos(-450,0)
+    t.color(("White"))
+    t.circle(100, 188)
+    t.left(90)
+    t.forward(193)
+
+
+def o():
+    t1.speed(20)
+    t1.penup()
+    t1.setpos(-250, -50)
+    t1.pendown()
+    t1.pensize(30)
+    t1.color("White")
+    t1.circle(100)
+def o1():
+    t1.speed(20)
+    t1.penup()
+    t1.setpos(-250, -50)
+    t1.pendown()
+    t1.pensize(30)
+    t1.color("White")
+    t1.circle(100)
+def g():
+    t2.speed(20)
+    t2.penup()
+    t2.setpos(-100,50)
+    t2.pendown()
+
+    t2.pensize(30)
+    t2.color("White")
+    t2.circle(-80,-180)
+    t2.right(90)
+    t2.forward(50)
+    t2.right(90)
+    t2.forward(50)
+    t2.right(90)
+    t2.forward(50)
+    t2.right(90 )
+    t2.forward(50)
+def g2():
+    t2.speed(20)
+    t2.penup()
+    t2.setpos(-350,50)
+    t2.pendown()
+
+    t2.pensize(30)
+    t2.color("White")
+    t2.circle(-80,-180)
+    t2.right(90)
+    t2.forward(50)
+    t2.right(90)
+    t2.forward(50)
+    t2.right(90)
+    t2.forward(50)
+    t2.right(90 )
+    t2.forward(50)
+
+def u():
+    t3.speed(20)
+    t3.penup()
+    t3.setpos(20,50)
+    t3.pendown()
+
+    t3.color("White")
+    t3.pensize(30)
+    t3.right(90)
+    t3.forward(150)
+    t3.circle(80,180)
+    t3.forward(150)
+
+
+def u2():
+    t1.speed(20)
+    t1.penup()
+    t1.setpos(-250,50)
+    t1.pendown()
+
+    t1.color("White")
+    t1.pensize(30)
+    t1.right(90)
+    t1.forward(150)
+    t1.circle(80,180)
+    t1.forward(150)
+def u3():
+    t3.speed(20)
+    t3.penup()
+    t3.setpos(20,50)
+    t3.pendown()
+
+    t3.color("White")
+    t3.pensize(30)
+    t3.right(90)
+    t3.forward(150)
+    t3.circle(80,180)
+    t3.forward(150)
+def u3():
+    t3.speed(20)
+    t3.penup()
+    t3.setpos(+160,50)
+    t3.pendown()
+
+    t3.color("White")
+    t3.pensize(30)
+    t3.right(90)
+    t3.forward(150)
+    t3.circle(80,180)
+    t3.forward(150)
+def u4():
+    t3.speed(20)
+    t3.penup()
+    t3.setpos(-175,50)
+    t3.pendown()
+
+    t3.color("White")
+    t3.pensize(30)
+    t3.right(90)
+    t3.forward(150)
+    t3.circle(80,180)
+    t3.forward(150)
+def u5():
+    t3.speed(20)
+    t3.penup()
+    t3.setpos(+150,50)
+    t3.pendown()
+
+    t3.color("White")
+    t3.pensize(30)
+    t3.right(90)
+    t3.forward(150)
+    t3.circle(80,180)
+    t3.forward(150)
+
+def m():
+    t4.speed(20)
+    t4.pensize(30)
+    t4.color("White")
+    t4.setpos(300,40)
+
+
+    t4.right(90)
+    t4.forward(150)
+    t4.backward(150)
+
+    t4.left(45)
+    t4.forward(90)
+
+    t4.left(95)
+    t4.forward(90)
+
+    t4.right(140)
+    t4.forward(150)
+
+def n():
+    t.speed(20)
+    t.setpos(-50,20)
+    t.pensize(30)
+    t.color("White")
+    t.right(90)
+    t.forward(120)
+    t.backward(120)
+
+    t.left(40)
+    t.forward(155)
+
+    t.left(139)
+    t.forward(120)
+def n2():
+    t6.speed(20)
+    t6.setpos(150, -50)
+    t6.pensize(30)
+    t6.color("White")
+    t6.right(180)
+    t6.forward(120)
+    t6.backward(120)
+
+    t6.left(40)
+    t6.forward(155)
+
+    t6.left(139)
+    t6.forward(120)
+
+def n3():
+    t.speed(20)
+    t.setpos(-200,20)
+    t.pensize(30)
+    t.color("White")
+    t.right(90)
+    t.forward(120)
+    t.backward(120)
+
+    t.left(40)
+    t.forward(155)
+
+    t.left(139)
+    t.forward(120)
+def k():
+    t.speed(20)
+    t.penup()
+    t.setpos(-350,50)
+    t.pendown()
+    t.pensize(30)
+    t.color("White")
+
+    t.right(90)
+    t.forward(200)
+    t.backward(100)
+    t.left(45)
+    t.forward(100)
+    t.backward(100)
+    t.left(80)
+    t.forward(100)
+def l():
+    t1.setpos(100,50)
+    t1.speed(20)
+    t1.penup()
+    t1.pensize(30)
+    t1.color("White")
+    t1.pendown()
+    t1.right(90)
+    t1.forward(170)
+    t1.left(90)
+    t1.forward(100)
+
+def l2():
+    t4.setpos(-200,50)
+    t4.speed(20)
+    t4.penup()
+    t4.pensize(30)
+    t4.color("White")
+    t4.pendown()
+    t4.right(90)
+    t4.forward(170)
+    t.left(90)
+    t.forward(100)
+def T():
+    t7.speed(20)
+    t7.penup()
+    t7.setpos(-25,140)
+    t7.pensize(30)
+    t7.color("White")
+    t7.pendown()
+    t7.forward(150)
+    t7.backward(75)
+    t7.right(90)
+    t7.forward(150)
+
+def s():
+    t.speed(20)
+    t.pensize(30)
+    t.color("White")
+
+    t.penup()
+    t.setpos(-150, 50)
+    t.pendown()
+
+    t.forward(30)
+    t.backward(39)
+
+    t.circle(-90, -185)
+    t.circle(90, -250)
+for x in range(40):
+    d()
+
+    o()
+
+    g()
+
+    u()
+
+    m()
+    temizle()
+
+    g2()
+
+    u2()
+
+    n()
+
+    u3()
+
+    n2()
+    temizle()
+
+    k()
+
+    u4()
+
+    T()
+
+    l()
+
+    u5()
+    temizle()
+
+    o()
+
+    l2()
+
+    s()
+
+    u5()
+
+    n3()
+    temizle()
+
+
+    turtle.width(7)
+    colors = ["#f5ac2f", "#279cf5", "#d820f5", "#a2f52f", "#f527c1"]
+
+
+    def draw_ankita(i, x, y):
+        turtle.pencolor("linen")
+        turtle.color(colors[i % 7])
+        turtle.lt(70)
+        turtle.penup()
+        turtle.goto(x, y)
+        turtle.pendown()
+        turtle.circle(22)
+        turtle.end_fill()
+
+
+    def ballon(x, y):
+        turtle.pensize(4)
+        for i in range(5):
+            draw_ankita(i, x, y)
+
+
+    def f1():
+        for i in range(7):
+            turtle.pensize(5)
+            turtle.pencolor('light blue')
+            turtle.color(colors[i % 19])
+            turtle.begin_fill()
+            turtle.left(330)
+            turtle.forward(55)
+            turtle.begin_fill()
+            turtle.rt(110)
+            turtle.circle(33)
+            turtle.end_fill()
+            turtle.rt(11)
+            turtle.backward(33)
+            turtle.end_fill()
+
+
+    def cake(x, y):
+        turtle.fd(x)
+        turtle.rt(90)
+        turtle.fd(y)
+        turtle.rt(90)
+        turtle.fd(x)
+        turtle.rt(90)
+        turtle.fd(y)
+
+
+    def move(x, y):
+        turtle.up()
+        turtle.setposition(0, 0)
+        turtle.setheading(90)
+        turtle.rt(90)
+        turtle.fd(x)
+        turtle.lt(90)
+        turtle.fd(y)
+        turtle.pendown()
+
+
+    def mov(x, y):
+        turtle.up()
+        turtle.setposition(0, 0)
+        turtle.setheading(90)
+        turtle.lt(90)
+        turtle.fd(x)
+        turtle.rt(90)
+        turtle.fd(y)
+        turtle.pendown()
+
+
+    def A(size):
+        turtle.rt(19)
+        turtle.forward(size)
+        turtle.rt(141)
+        turtle.fd(size)
+        turtle.backward(size / 2)
+        turtle.rt(105)
+        turtle.fd(int(size / 3))
+
+
+    def B(size):
+        turtle.forward(size)
+        turtle.rt(90)
+        for i in range(18):
+            turtle.rt(9)
+            turtle.fd(size // 20)
+        for i in range(18):
+            turtle.rt(size // 5)
+            turtle.backward(size // 20)
+
+
+    def D(size):
+        turtle.fd(size)
+        turtle.rt(90)
+        turtle.fd(size // 10)
+        for i in range(13):
+            turtle.rt(13)
+            turtle.fd(size // 8)
+
+
+    def E(size):
+        turtle.rt(90)
+        turtle.fd(int(size / 3))
+        turtle.back(int(size / 3))
+        turtle.left(90)
+        turtle.fd(size / 2)
+        turtle.rt(90)
+        turtle.fd(int(size / 3))
+        turtle.back(int(size / 3))
+        turtle.lt(90)
+        turtle.fd(size / 2)
+        turtle.rt(90)
+        turtle.fd(int(size / 3))
+
+
+    def H(size):
+        turtle.fd(size)
+        turtle.backward(size // 2)
+        turtle.rt(90)
+        turtle.fd(size // 2)
+        turtle.lt(90)
+        turtle.fd(size // 2)
+        turtle.backward(size)
+
+
+    def I(size):
+        turtle.fd(size)
+        turtle.rt(90)
+        turtle.circle(size // 8)
+
+
+    def L(size):
+        turtle.rt(90)
+        turtle.fd(int(size / 2))
+        turtle.back(int(size / 2))
+        turtle.lt(90)
+        turtle.fd(size)
+
+
+    def N(size):
+        turtle.fd(size)
+        turtle.rt(150)
+        turtle.fd(size + int(size / 6))
+        turtle.lt(150)
+        turtle.fd(size)
+
+
+    def P(size):
+        turtle.fd(size)
+        turtle.rt(90)
+        turtle.fd(size // 8)
+        for i in range(8):
+            turtle.rt(20)
+            turtle.fd(size // 9)
+
+
+    def R():
+        turtle.fd(60)
+        turtle.rt(90)
+        turtle.fd(7)
+        for i in range(15):
+            turtle.rt(12)
+            turtle.fd(3)
+        turtle.lt(120)
+        turtle.fd(40)
+
+
+    def S(size):
+        turtle.rt(90)
+        for i in range(0, 5):
+            if i < 3:
+                turtle.fd(size / 2)
+                turtle.lt(90)
+                if i == 2:
+                    turtle.rt(90)
+            else:
+                turtle.right(90)
+                turtle.fd(size / 2)
+
+
+    def T_1(size):
+        turtle.fd(size)
+        turtle.rt(90)
+        turtle.fd(size // 2)
+        turtle.backward(size // 2)
+
+
+    def Y(size):
+        turtle.fd(size)
+        turtle.left(60)
+        turtle.fd(size // 2)
+        turtle.backward(size // 2)
+        turtle.rt(90)
+        turtle.fd(size // 1.5)
+
+
+    turtle.speed(19)
+
+    mov(120, 30)
+    turtle.color("#f7b543")
+    # tech_habit.color(colors[8 % 5])
+    turtle.begin_fill()
+    cake(40, 180)
+    turtle.end_fill()
+    mov(110, 75)
+    turtle.color("#d152f7")
+    turtle.begin_fill()
+    cake(40, 160)
+    turtle.end_fill()
+    mov(100, 120)
+    turtle.color("#f54eb8")
+    turtle.begin_fill()
+    cake(40, 140)
+    turtle.end_fill()
+    mov(30, 170)
+    turtle.width(11)
+    turtle.pencolor("red")
+    turtle.circle(7)
+    move(180, 307)
+    mov(0, 0)
+    ballon(-490, 200)
+    ballon(490, 200)
+    ballon(183, -150)
+    ballon(-133, -150)
+
+    turtle.speed(7)
+    turtle.width(9)
+    turtle.pencolor("#319df5")
+
+
+    turtle.pencolor("cyan")
+    turtle.width(13)
+    mov(260, -80)
+    H(100)
+    turtle.width(7)
+    mov(190, -80)
+    A(65)
+    mov(135, -80)
+    P(60)
+    mov(100, -80)
+    P(60)
+    mov(52, -80)
+    Y(60)
+    mov(28, -80)
+    B(60)
+    move(12, -80)
+    I(60)
+    move(36, -80)
+    R()
+    move(80, -80)
+    T_1(100)
+    move(102, -80)
+    H(60)
+    move(150, -80)
+    turtle.pencolor('hotpink')
+    D(200)
+    move(160, -80)
+    A(60)
+    move(220, -80)
+    Y(60)
+
+turtle.exitonclick()
+
+
+
+
+
+
+
+
